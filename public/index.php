27248278f0d7aec54fedcaaede20c41d6fb7f620
Lavender <gercermagden@gmail.com>
2025-04-02 00:57:04 -0500

    feat(estructura): crear base de .htaccess, dashboard.php e index.php
    - Se creó la estructura base del archivo `.htaccess` para configuración de rutas y reglas del servidor.
    - Se añadió el archivo `dashboard.php` con la estructura inicial para la interfaz principal de la aplicación.
    - Se creó el archivo `index.php` como página de inicio (landing page) con la configuración básica.

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php include __DIR__ . '/../app/views/template/header.php'; ?>
+<?php include __DIR__ . '/../app/views/includes/navbar.php'; ?>
+
+<!-- Contenido principal aquí -->
+
+<?php include __DIR__ . '/../app/views/includes/footerbar.php'; ?>
+<?php include __DIR__ . '/../app/views/template/footer.php'; ?>
