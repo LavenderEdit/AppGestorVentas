003c2ebc9a51bb3db457ed58a8f949b211bc7130
Lavender <gercermagden@gmail.com>
2025-04-04 20:15:44 -0500

    chore: agregar dependencias y configuración de autoloader en composer.json

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit19f438c0ea494d8420226cc27f4813c2::getLoader();
+return ComposerAutoloaderInita1e3b2fa04e1142019c4a6493256258c::getLoader();
